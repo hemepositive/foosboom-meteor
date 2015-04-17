0270a30c5d23ed44b613a4af471bd34bca502c93
Jonathan White <hemepositive@gmail.com>
2015-04-17 14:36:44 -0400

    games hisWay

--- client/views/games.js
+++ client/views/games.js
@@ -1,5 +1,52 @@
 Template.games.helpers({
 	games: function() {
-	return Games.find();
+		return Games.find();
 	},
+	teams: function() {
+		return Teams.find();
+	},
+	isCreatingGame: function() {
+		return Session.get('isCreatingGame');
+	}
 });
+
+Template.teams.events({
+  
+  'click a.create': function(e, tpl){
+  	e.preventDefault();
+    Session.set('isCreatingGame', true);
+  },
+
+  'click a.cancel': function(e, tpl){
+  	e.preventDefault();
+    Session.set('isCreatingGame', null);
+  },
+
+   'submit form.create-game': function(e, tpl){
+  	e.preventDefault();
+  	// get teams from selections
+  	var team1 = {
+  		id: tpl.$("select[name='teamOne']").val(),
+  		name: tpl.$("select[name='teamOne'] option:selected").text(),
+  		score:0
+  	}
+  	var team2 = {
+  		id: tpl.$("select[name='teamTwo']").val(),
+  		name: tpl.$("select[name='teamTwo'] option:selected").text(),
+  		score:0
+  	}
+  	// create a gome
+ 	var game = {
+    	completed: false,
+    	createdAt: new Date(),
+    	teams: [team1, team2]
+	};
+	// insert game
+  	var gameId = Games.insert(game);
+
+  	Teams.update({_id: team1.id}, {$addToSet: { gameIds: gameId}});
+    Teams.update({_id: team2.id}, {$addToSet: { gameIds: gameId}});
+
+    Session.set('isCreatingGame', null);
+  }
+}); //events
