e5d41808ab23b2ed1e7a63a5fe156bdc18e6949b
Jonathan White <hemepositive@gmail.com>
2015-04-22 14:30:02 -0400

    fixed

--- client/views/team.js
+++ client/views/team.js
@@ -1,47 +1,51 @@
 Template.team.helpers({
-	isEditingTeam: function(){
-		// COOL: if this._id present then BOOL === true
-		return Session.get('editedTeamId') === this._id;
-  	},
-
+  isEditingTeam: function(){
+    return Session.get('isEditingTeam') === this._id;
+  }
 });
 
 Template.team.events({
-    'click a.remove': function(e,tpl) {
- 		 e.preventDefault();
- 		 Teams.remove(this._id);
-    },
-
-    "click a.edit": function(e, tpl){
-    	e.preventDefault();
-      Session.set('editedTeamId', this._id);
-	 },
-
-    'click a.cancel': function(e, tpl){
-  		e.preventDefault();
-    	Session.set('editedTeamId', null);
+  "click a.edit": function(e, tpl){
+    e.preventDefault();
+    Session.set('isEditingTeam', this._id);
   },
-    'submit form.edit-team': function(e, tpl){
-      e.preventDefault;
-      var teamName = tpl.$("$input[name='name]").val();
-      var self = this;
-      if (teamName.length){
-        Teams.update(this._id, {$set: {name: teamName}},
-          function(error) {
-            if(!error){
-              var games = Games.find({_id: {$in: self.gameIds}});
-              if(games.count()){
-                _(games.fetch()).each(function(game) {
-                  var team = _(game.teams).findWhere({id: self._id});
-                  if (team != null) {
-                    team.name = teamName;
-                    Games.update({_id: game._id}, {$set: {teams: game.teams}})
-                  }
-                });
+ 
+  "submit form.edit-team": function(e, tpl){
+    e.preventDefault();
+ 
+    var teamName = tpl.$("input[name='name']").val();
+    var self = this;
+
+    if(teamName.length){
+      Teams.update(this._id, {$set: {name: teamName}}, function(error){
+
+        if(typeof error === "undefined" || error === null){
+
+          // Update games this team is a part of
+          var games = Games.find({_id: {$in: self.gameIds}});
+          if(games.count()){
+            _(games.fetch()).each(function(game){
+              var team = _(game.teams).findWhere({id: self._id});
+              if(team != null){
+                team.name = teamName;
+                Games.update({_id: game._id}, {$set: {teams: game.teams}})
               }
-            }
-          });
-        Session.set("editedTeamId", null);
-      }
-    },
-  });
+            });
+          }
+        }
+      });
+
+      Session.set('isEditingTeam', null);
+    }
+  },
+ 
+  "click a.cancel": function(e, tpl){
+    e.preventDefault();
+    Session.set('isEditingTeam', null);
+  },
+ 
+  'click a.remove': function(e, tpl){
+    e.preventDefault();
+    Teams.remove(this._id);
+  }
+});
