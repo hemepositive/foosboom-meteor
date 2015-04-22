06a4c89d59e7e7b78894ae73cc8a684a08c34fdb
Jonathan White <hemepositive@gmail.com>
2015-04-22 14:22:30 -0400

    not updating db

--- client/views/team.js
+++ client/views/team.js
@@ -7,25 +7,41 @@ Template.team.helpers({
 });
 
 Template.team.events({
-	'click a.remove': function(e,tpl) {
- 		e.preventDefault();
- 		Teams.remove(this._id);
+    'click a.remove': function(e,tpl) {
+ 		 e.preventDefault();
+ 		 Teams.remove(this._id);
     },
 
-  	"click a.edit": function(e, tpl){
+    "click a.edit": function(e, tpl){
     	e.preventDefault();
-    	Session.set('editedTeamId', this._id);
-	},
+      Session.set('editedTeamId', this._id);
+	 },
 
-	'click a.cancel': function(e, tpl){
+    'click a.cancel': function(e, tpl){
   		e.preventDefault();
     	Session.set('editedTeamId', null);
-  	},
-
-	'submit form.edit-team': function(e, tpl){
-  		e.preventDefault();
-    	var newData = tpl.$('input[name=name]').val();
-    	Teams.update(this._id, {$set: {name: newData}});
-      Session.set('editedTeamId', null);
-    }, //submit
-});
+  },
+    'submit form.edit-team': function(e, tpl){
+      e.preventDefault;
+      var teamName = tpl.$("$input[name='name]").val();
+      var self = this;
+      if (teamName.length){
+        Teams.update(this._id, {$set: {name: teamName}},
+          function(error) {
+            if(!error){
+              var games = Games.find({_id: {$in: self.gameIds}});
+              if(games.count()){
+                _(games.fetch()).each(function(game) {
+                  var team = _(game.teams).findWhere({id: self._id});
+                  if (team != null) {
+                    team.name = teamName;
+                    Games.update({_id: game._id}, {$set: {teams: game.teams}})
+                  }
+                });
+              }
+            }
+          });
+        Session.set("editedTeamId", null);
+      }
+    },
+  });
